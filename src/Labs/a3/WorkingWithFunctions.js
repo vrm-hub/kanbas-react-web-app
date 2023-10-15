dbfdbfed4273fced227b4b3bef6468d3968cdb7d
vrm-hub <vinay97.pro@gmail.com>
2023-10-15 16:59:45 -0400

    a3 labs - up till functions

---
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,13 @@
+import ES5Functions from "./ES5Functions";
+
+function WorkingWithFunctions() {
+    console.log('Working with functions');
+    return (
+        <div>
+            <ES5Functions/>
+
+        </div>
+    );
+}
+
+export default WorkingWithFunctions
