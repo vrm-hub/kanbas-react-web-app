cfbd3c56862f42b46d63f7b3b217ca08d940d1ef
vrm-hub <vinay97.pro@gmail.com>
2023-10-19 23:44:30 -0400

    fixed Account font size

--- src/Kanbas/index.jsx
+++ src/Kanbas/index.jsx
@@ -13,14 +13,14 @@ function kanbas() {
                     <Route
                         path="Account"
                         element={
-                            <div style={{float: "right", width: "calc(100% -  130px)"}}>
-                                Account
+                            <div style={{left: "110px", position: "relative"}}>
+                                <h1>Account</h1>
                             </div>
                         }
                     />
                     <Route path="Dashboard" element={<Dashboard/>}/>
                     <Route path="Courses/:courseId/*" element={<Courses/>}/>
-                    <Route path="Calendar" element={<h1 style={{left: "100px", position: "relative"}}>Calendar</h1>}/>
+                    <Route path="Calendar" element={<h1 style={{left: "110px", position: "relative"}}>Calendar</h1>}/>
 
                 </Routes>
             </div>
